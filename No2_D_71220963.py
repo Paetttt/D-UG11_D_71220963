41e88fb740bce5417754bcc4a5870c4eb30a5fab
Paetttt <118429357+Paetttt@users.noreply.github.com>
2022-12-01 16:49:01 +0700

    Add files via upload

--- No2_D_71220963.py
+++ No2_D_71220963.py
@@ -1,1 +1,10 @@
-if kata <= 
+def testhuruf () :
+    kata=(input("Masukkan kata :"))
+    list=(kata)
+    len(kata)
+    if(len(kata)%2==0) :
+        print("Huruf paling ujung pada kata", kata, "adalah", (kata[-3:]))
+    else :
+        print("Huruf paling ujung pada kata", kata, "adalah",(kata[:3]))
+    return (kata)
+testhuruf()
